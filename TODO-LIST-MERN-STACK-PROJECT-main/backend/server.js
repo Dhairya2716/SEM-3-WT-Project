1e363d92c031764ce29466dafb7147eed0adacc3
Dhairya2716 <140948481+Dhairya2716@users.noreply.github.com>
2024-10-09 19:28:54 +0530

    Update server.js

--- TODO-LIST-MERN-STACK-PROJECT-main/backend/server.js
+++ TODO-LIST-MERN-STACK-PROJECT-main/backend/server.js
@@ -7,22 +7,18 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to your MongoDB database (replace with your database URL)
 mongoose.connect("mongodb+srv://Dhairya:Dh%40iry%402716@cluster0.3sfum.mongodb.net/server");
 
-// Check for database connection errors
 mongoose.connection.on("error", (error) => {
     console.error("MongoDB connection error:", error);
 });
 
-// Get saved tasks from the database
 app.get("/getTodoList", (req, res) => {
     TodoModel.find({})
         .then((todoList) => res.json(todoList))
         .catch((err) => res.json(err))
 });
 
-// Add new task to the database
 app.post("/addTodoList", (req, res) => {
     TodoModel.create({
         task: req.body.task,
@@ -33,7 +29,6 @@ app.post("/addTodoList", (req, res) => {
         .catch((err) => res.json(err));
 });
 
-// Update task fields (including deadline)
 app.post("/updateTodoList/:id", (req, res) => {
     const id = req.params.id;
     const updateData = {
@@ -46,7 +41,6 @@ app.post("/updateTodoList/:id", (req, res) => {
         .catch((err) => res.json(err));
 });
 
-// Delete task from the database
 app.delete("/deleteTodoList/:id", (req, res) => {
     const id = req.params.id;
     TodoModel.findByIdAndDelete({ _id: id })
